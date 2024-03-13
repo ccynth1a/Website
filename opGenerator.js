dcbc3b33d576394700caddb5e43c86a8573d864f
ccynth1a <charlottetorvell@protonmail.com>
2024-03-13 00:16:45 +0000

    styling changes complete(?)

--- opGenerator.js
+++ opGenerator.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
   ];
     function operatorImageUpdate(randomOp) {
       let imgUrl;
-      imgUrl = "Assets/" + randomOp + ".png";
+      ﻿imgUrl = "Assets/" + randomOp + ".png";
     //Legacy Code that I've kept in because its fucking hilarious
     /*switch (randomOp) {
         // Attacking Operators
